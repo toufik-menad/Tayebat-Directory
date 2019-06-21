3d0237f46b09fbb79f24e699a44b7a8aa4a8835e
Toufik <toufik.menad@gmail.com>
2019-06-20 18:50:55 -0700

    tests for Restemplate

--- core/src/main/java/core/Application.java
+++ core/src/main/java/core/Application.java
@@ -3,13 +3,18 @@
  */
 package core;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author MENAD
@@ -21,9 +26,34 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 //@EnableWebMvc
 //@EnableJpaRepositories({"com.*" })
 //@EntityScan({"com.*" })
-public class Application  {
+public class Application  implements CommandLineRunner{
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * @param args
+     * @throws Exception
+     */
+    @Override
+    public void run(String... args) throws Exception {
+        
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response
+        = restTemplate.getForEntity("https://world.openfoodfacts.org/api/v0/product/3248710361005.json", String.class);
+        
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> map = objectMapper.readValue(response.getBody(), new TypeReference<HashMap<String,Object>>(){});
+        
+        JsonNode root = objectMapper.readTree(response.getBody());
+        JsonNode name = root.path("product");
+        
+        
+        
+        System.out.println(name.get("ingredients"));
+        
+        System.out.println(name.get("ingredients_text_with_allergens_fr"));
+
     }
 }
---
+++ core/src/main/java/core/com/entity/ItemEntity.java
@@ -0,0 +1,18 @@
+/*
+ *
+ *
+ * Copyright (c) 2018, CGI.
+ */
+package core.com.entity;
+
+/**
+ * @author T.Menad
+ */
+public class ItemEntity {
+    
+    private Product product;
+    private String code;
+    private int status;
+    private String status_verbose;
+
+}
