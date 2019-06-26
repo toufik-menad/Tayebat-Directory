4a9b7ad25c4fa00da319c90de0007c77cfacaf3f
Toufik <toufik.menad@gmail.com>
2019-06-25 19:41:38 -0700

    conflict resolved

--- RestServices/src/main/java/Application.java
+++ RestServices/src/main/java/Application.java
@@ -1,5 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 /**
  * @author T.Menad
  */
@@ -13,9 +23,35 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@EnableWebMvc
 //@EnableJpaRepositories({"com.*" })
 //@EntityScan({"com.*" })
-public class Application  {
+public class Application  implements CommandLineRunner{
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    
+    
+    
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public void run(String... args) throws Exception {
+        
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response
+        = restTemplate.getForEntity("https://world.openfoodfacts.org/api/v0/product/3248710361005.json", String.class);
+        
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> map = objectMapper.readValue(response.getBody(), new TypeReference<HashMap<String,Object>>(){});
+        
+        JsonNode root = objectMapper.readTree(response.getBody());
+        JsonNode name = root.path("product");
+        
+        
+        
+        System.out.println(name.get("ingredients"));
+        
+        System.out.println(name.get("ingredients_text_with_allergens_fr"));
+
     }
 }
